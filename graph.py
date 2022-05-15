765a599f8892f4738b000ac97d5e5fbf51a614e9
Richard Wells <rwells1703@gmail.com>
2022-05-15 17:07:07 +0100

    Reads curves from data file, plots and saves them as image files.

--- graph.py
+++ graph.py
@@ -1,1 +1,17 @@
-print("hello world")
+import csv
+from matplotlib import pyplot as plt
+import numpy as np
+
+with open("data\TestingData.csv") as file:
+    # Read the CSV file data
+    reader = csv.reader(file)
+
+    curves = []
+    # Store each line of the data into a list of curves
+    for row in reader:
+        curves.append([float(v) for v in row])
+    
+for i, c in enumerate(curves):
+    plt.plot(range(len(c)), c)
+    plt.savefig("figures\\curve{number}.jpg".format(number=i+1), format="jpg")
+    plt.close()
