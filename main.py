b16afff5487c08da0605d6657751e88cd60e4e00
Richard Wells <rwells1703@gmail.com>
2022-05-23 23:16:39 +0100

    Removed hard coded data used during debugging.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import data_load
 import svc
 import lp_generate
 import lp_solve
@@ -6,12 +5,7 @@ import lp_solve
 def main():
     lp_generate.create_scheduling_scripts()
 
-    # The predictions obtained from running the SVM with a high number of bagging estimators
-    curves = data_load.read_data("data\\TestingData.txt", "f*")
-    predictions = [1.0,1.0,0.0,0.0,1.0,1.0,0.0,1.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,1.0,1.0,1.0,1.0,1.0,0.0,1.0,0.0,0.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,1.0,1.0,0.0,1.0,0.0,1.0,0.0,1.0,1.0,1.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,1.0,0.0,0.0,1.0,1.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,1.0,1.0,1.0,1.0,1.0,1.0,0.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,0.0,0.0,0.0,1.0,1.0,1.0,0.0,0.0,1.0,0.0]
-    svc.save_testing_results(curves, predictions)
-
-    #predictions, curves = svm.predict_testing_data()
+    predictions, curves = svc.predict_testing_data()
 
     curve_indexes = svc.get_abormal_curves(predictions)
     all_hour_totals = lp_solve.get_curve_schedules(curve_indexes)
