37ebe24b91f5cb0c59a782a59ffe931e94199d9b
Richard Wells <rwells1703@gmail.com>
2022-05-23 21:59:07 +0100

    Renamed SVM to SVC to reflect exact method used.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import svm
+import svc
 import lp_generate
 import lp_solve
 
@@ -7,7 +7,7 @@ def main():
     #predictions = svm.predict_testing_data()
     # The predictions obtained from running the SVM with a high number of bagging estimators
     predictions = [1.0,1.0,0.0,0.0,1.0,1.0,0.0,1.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,1.0,1.0,1.0,1.0,1.0,0.0,1.0,0.0,0.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0,1.0,0.0,0.0,1.0,0.0,0.0,1.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,1.0,1.0,0.0,1.0,0.0,1.0,0.0,1.0,1.0,1.0,0.0,1.0,0.0,0.0,0.0,0.0,1.0,1.0,0.0,0.0,1.0,1.0,1.0,0.0,0.0,0.0,1.0,0.0,0.0,1.0,1.0,1.0,1.0,1.0,1.0,0.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,0.0,0.0,0.0,1.0,1.0,1.0,0.0,0.0,1.0,0.0]
-    curve_indexes = svm.get_abormal_curves(predictions)
+    curve_indexes = svc.get_abormal_curves(predictions)
     lp_solve.display_curve_schedules(curve_indexes)
 
 if __name__ == "__main__":
